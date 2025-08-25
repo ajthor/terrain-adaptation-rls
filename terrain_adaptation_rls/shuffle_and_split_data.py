c8ca51e8c076f8d001a2ad3f4ed8bcd1c570a5eb
William Ward <willward1912@gmail.com>
2025-08-25 08:53:45 -0500

    Update for jackal_0770 data.

--- terrain_adaptation_rls/shuffle_and_split_data.py
+++ terrain_adaptation_rls/shuffle_and_split_data.py
@@ -1,7 +1,7 @@
 import csv
 import os
 import torch
-from data.load_data import load_all_scenes, load_all_bluebonnet_scenes
+from data.load_data import load_all_scenes, load_hardware_scenes
 
 # Write a torch tensor to a CSV file.
 def write_csv(path, tensor_data):
@@ -11,15 +11,16 @@ def write_csv(path, tensor_data):
             writer.writerow(row)
 
 # Choose hardware or simulation.
-platform = 'bluebonnet'
+mode = 'hardware'
+platform = 'jackal_0770'
 
 # Load all scene data as a dictionary
-if platform == 'sim':
+if mode == 'sim':
     scene_data = load_all_scenes()
     scenes = [0, 1, 2, 3, 4, 5, 6, 7]
-elif platform == 'bluebonnet':
-    scene_data = load_all_bluebonnet_scenes()
-    scenes = ['grass', 'gravel', 'mulch']
+elif mode == 'hardware':
+    scenes = ['gym-floor2']
+    scene_data = load_hardware_scenes(scenes, platform)
 
 # Choose random seeds.
 seeds = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 42]
@@ -34,9 +35,9 @@ for seed in seeds:
     for scene_index in scenes:
 
         # Extract the inputs and targets. 
-        if platform == 'sim':
+        if mode == 'sim':
             scene_str = f"scene{scene_index}"
-        elif platform == 'bluebonnet':
+        elif mode == 'hardware':
             scene_str = f"{scene_index}"
 
         scene_input, scene_target = scene_data[scene_str]
@@ -57,10 +58,10 @@ for seed in seeds:
         test_target = scene_target[test_indices]
 
         # Save the data to CSV files
-        if platform == 'sim':
+        if mode == 'sim':
             save_path = f"terrain_adaptation_rls/data_split/seed_{seed}/scene{scene_index}"
-        elif platform == 'bluebonnet':
-            save_path = f"terrain_adaptation_rls/data_split/seed_{seed}/bluebonnet_{scene_index}"
+        elif mode == 'hardware':
+            save_path = f"terrain_adaptation_rls/data_split/seed_{seed}/{platform}_{scene_index}"
         os.makedirs(save_path, exist_ok=True)
         write_csv(f"{save_path}/train_input.csv", train_input)
         write_csv(f"{save_path}/train_target.csv", train_target)
