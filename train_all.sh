3ce93db2d3c997ca102eaf6d00ba608dc6dd331f
William Ward <willward1912@gmail.com>
2025-09-17 12:13:14 -0500

    Update train_all.sh for release.

--- train_all.sh
+++ train_all.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 0 --platform jackal_0770
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 1 --platform jackal_0770
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 2 --platform jackal_0770
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 3 --platform jackal_0770
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 4 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 0 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 1 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 2 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 3 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 4 --platform jackal_0770
 
 python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 0 --platform warty
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 1 --platform warty
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 2 --platform warty
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 3 --platform warty
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 4 --platform warty
+python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 1 --platform warty
+python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 2 --platform warty
+python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 3 --platform warty
+python3 terrain_adaptation_rls/train.py --grad 10000 --model neural_ode --seed 4 --platform warty
 
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 0 --platform jackal_0770
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 1 --platform jackal_0770
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 2 --platform jackal_0770
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 3 --platform jackal_0770
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 4 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 0 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 1 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 2 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 3 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 4 --platform jackal_0770
 
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 0 --platform warty
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 1 --platform warty
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 2 --platform warty
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 3 --platform warty
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 4 --platform warty
+python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 0 --platform warty
+python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 1 --platform warty
+python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 2 --platform warty
+python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 3 --platform warty
+python3 terrain_adaptation_rls/train.py --grad 10000 --model function_encoder --seed 4 --platform warty
 
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 0 --platform jackal_0770
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 1 --platform jackal_0770
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 2 --platform jackal_0770
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 3 --platform jackal_0770
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 4 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 0 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 1 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 2 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 3 --platform jackal_0770
+python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 4 --platform jackal_0770
 
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 0 --platform warty
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 1 --platform warty
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 2 --platform warty
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 3 --platform warty
-# python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 4 --platform warty
+python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 0 --platform warty
+python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 1 --platform warty
+python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 2 --platform warty
+python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 3 --platform warty
+python3 terrain_adaptation_rls/train.py --grad 10000 --model maml --seed 4 --platform warty
